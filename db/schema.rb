8d9159883d0c1abd8c42aae44d1756693f9a0f40
Greg Benn <gbenn07@gmail.com>
2018-09-07 13:30:49 -0400

    Generated Customers, and DB migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_143145) do
+ActiveRecord::Schema.define(version: 2018_09_07_172822) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
