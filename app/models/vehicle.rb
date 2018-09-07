0466879f200d6bc205ca6e4bc5dd945d48d1006a
Greg Benn <gbenn07@gmail.com>
2018-09-07 17:38:47 -0400

    Added 'pry' satisfied rubocop.

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Vehicle < ApplicationRecord
   belongs_to :customer
 end
--- db/migrate/20180907182725_create_vehicles.rb
+++ db/migrate/20180907182725_create_vehicles.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateVehicles < ActiveRecord::Migration[5.2]
   def change
     create_table :vehicles do |t|
