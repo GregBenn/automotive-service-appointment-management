daa1c9d16d2ac2117367475adf4dac4456295d7f
Greg Benn <gbenn07@gmail.com>
2018-09-07 21:08:16 -0400

    Modified sessions routes and nested vehicles routes inside customers routes.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,9 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  def create
-  end
+  def create; end
 
-  def destroy
-  end
+  def destroy; end
 
-  def new
-  end
+  def new; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'sessions/new'
-  get 'static_pages/home'
-  resources :appointments
-  resources :vehicles
-  resources :customers
+  get "/signin", to: "sessions#new", as: "signin"
+  post "/sessions", to: "sessions#create", as: "sessions"
+  delete "/sessions/", to: "sessions#destroy"
+
+  get "static_pages/home"
+
   resources :users
+  resources :customers do
+    resources :vehicles
+  end
+  resources :appointments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
