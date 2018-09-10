24535940a784edb89c87f0754bb042d8e0e2882c
Greg Benn <gbenn07@gmail.com>
2018-09-10 13:12:24 -0400

    Added basic CRUD actions to users_controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+private
+
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
