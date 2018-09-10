9e346a6223b881c35e7658745d3a26e991ff70ee
Greg Benn <gbenn07@gmail.com>
2018-09-10 13:20:02 -0400

    Added Sessions functionality.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
-  def create; end
+  def create
+    user = User.find_by_id(params[:user][:name])
+    session[:user_id] = user.id
+    redirect_to user_path(user)
+  end
 
-  def destroy; end
+  def destroy
+    session.delete :user_id if session[:user_id]
+    redirect_to root_path
+  end
 
-  def new; end
+  def new
+    @user = User.new
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    @user.save
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      redirect_to new_user_path
+    end
   end
 
   def show
