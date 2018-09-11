0aaa30955980f86cfa0cbaeea2dd69eca7f74ad4
Greg Benn <gbenn07@gmail.com>
2018-09-11 10:56:55 -0400

    refactored with #find_user and before_action for user show, edit, and update controller methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :find_user, only: %i[show edit update]
+
   def new
     @user = User.new
   end
@@ -19,16 +21,11 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  def show; end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user)
   end
@@ -37,5 +34,9 @@ private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
