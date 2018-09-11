d8b63014875784894681443061a8b28069488122
Greg Benn <gbenn07@gmail.com>
2018-09-11 12:31:34 -0400

    added cruc functionality for Customer controller actions and views.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,45 @@
 # frozen_string_literal: true
 
 class CustomersController < ApplicationController
+  before_action :require_logged_in, only: %i[index show edit update]
+  before_action :find_customer, only: %i[show edit update]
+
+  def new
+    @customer = Customer.new
+  end
+
+  def index
+    @customers = Customer.all
+  end
+
+  def create
+    customer = Customer.new(customer_params)
+    if customer.save
+      session[:customer_id] = customer.id
+      redirect_to customer_path(customer)
+    else
+      redirect_to new_customer_path
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    @customer.update(customer_params)
+    redirect_to customer_path(@customer)
+  end
+
+private
+
+  def customer_params
+    params.require(:customer).permit(:name,
+                                 :email,
+                                 :phone_number)
+  end
+
+  def find_customer
+    @customer = Customer.find(params[:id])
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,6 @@
 class Customer < ApplicationRecord
   has_many :vehicles
   has_many :appointments, through: :vehicles
+
+  # need to add a phone number custom validator
 end
