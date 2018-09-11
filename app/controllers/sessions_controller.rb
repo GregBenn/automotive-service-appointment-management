91e0a83e6895fbf7dd550341d9fa7e3b504ebcf2
Greg Benn <gbenn07@gmail.com>
2018-09-11 11:41:18 -0400

    Changed #create to find_by email instead of find_by id in sessions_controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class SessionsController < ApplicationController
   def create
-    user = User.find_by_id(params[:user][:email])
+    user = User.find_by(email: params[:user][:email])
     session[:user_id] = user.id
     redirect_to user_path(user)
   end
