87337e6c71a7702b0c13bdb16a2ab00f5cf8dc0f
Greg Benn <gbenn07@gmail.com>
2018-09-12 15:58:25 -0400

    Building out form functionality and controller actions.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,7 +4,8 @@ class AppointmentsController < ApplicationController
 
   def new
     @appointment = Appointment.new
-    binding.pry
+    # binding.pry
     @customer = Customer.find_by(id: params[:customer_id])
+    @vehicle = Vehicle.find_by(id: params[:vehicle_id])
   end
 end
