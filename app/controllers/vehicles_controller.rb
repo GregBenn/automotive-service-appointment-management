17de610db13141d75c188cdb992f68e86d3019a6
Greg Benn <gbenn07@gmail.com>
2018-09-12 10:22:50 -0400

    Basic controller actions, used #merge to add customer_id to #vehicle_params.

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,4 +1,54 @@
 # frozen_string_literal: true
 
 class VehiclesController < ApplicationController
+  before_action :find_vehicle, only: %i[show edit update]
+
+  def new
+    @customer = Customer.find_by(id: params[:customer_id])
+    @vehicle = Vehicle.new
+  end
+
+  def index
+    @vehicles = Vehicle.all
+  end
+
+  def create
+    # binding.pry
+    vehicle = Vehicle.new(vehicle_params)
+    if vehicle.save
+      redirect_to customer_path(current_customer_id)
+    else
+
+      redirect_to new_customer_vehicle_path
+    end
+  end
+
+  def show
+
+  end
+
+  def edit; end
+
+  def update
+    @vehicle.update(vehicle_params)
+    redirect_to vehicle_path(@vehicle)
+  end
+
+private
+
+  def vehicle_params
+    params.require(:vehicle).permit(
+      :year,
+      :make,
+      :model,
+      :mileage).merge(customer_id: current_customer_id)
+  end
+
+  def find_vehicle
+    @vehicle = Vehicle.find(params[:id])
+  end
+
+  def current_customer_id
+    params[:customer_id]
+  end
 end
