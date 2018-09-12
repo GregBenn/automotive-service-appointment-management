760cdd29454d9a313091e0947bcccc8b30ac05be
Greg Benn <gbenn07@gmail.com>
2018-09-12 14:43:47 -0400

    Adding functionality to create appointments.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class AppointmentsController < ApplicationController
+
+  def new
+    @appointment = Appointment.new
+    binding.pry
+    @customer = Customer.find_by(id: params[:customer_id])
+  end
 end
