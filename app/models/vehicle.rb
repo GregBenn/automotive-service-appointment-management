346172acbbab55430643dc9b1eac4a36ed14f5c3
Greg Benn <gbenn07@gmail.com>
2018-09-12 09:34:45 -0400

    #vehicle_description concatenates year make and model of vehicle.

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -4,4 +4,8 @@ class Vehicle < ApplicationRecord
   belongs_to :customer
   has_many :appointments
   has_many :users, through: :appointments
+
+  def vehicle_description
+    "#{vehicle.year} #{vehicle.make} #{vehicle.model}"
+  end
 end
