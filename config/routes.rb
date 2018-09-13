fd40921da843a4a003ca788594903c05100d047b
Greg Benn <gbenn07@gmail.com>
2018-09-13 10:43:43 -0400

    Completed logout functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@
 Rails.application.routes.draw do
   get "/signin", to: "sessions#new", as: "signin"
   post "/sessions", to: "sessions#create", as: "sessions"
-  delete "/sessions/", to: "sessions#destroy"
+  delete "/sessions/", to: "sessions#destroy", as: "logout"
+
 
   root "static_pages#home"
 
