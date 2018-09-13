af6ddeb93b2d5d5ba007b1660f9b1a5c4bf1bf6e
Greg Benn <gbenn07@gmail.com>
2018-09-13 11:51:46 -0400

    Added ActiveRecord scope functionality.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,4 +3,13 @@
 class Appointment < ApplicationRecord
   belongs_to :vehicle
   belongs_to :user
+  scope :pending, -> { where(status: "pending") }
+  scope :open, -> { where(status: "open") }
+  scope :closed, -> { where(status: "closed") }
+  scope :cancelled, -> { where(status: "cancelled") }
+
+
+  # def self.pending
+  #   where(status: "pending")
+  # end
 end
