8f02d040613177ec9b202203b56f0b820b6fc51a
Greg Benn <gbenn07@gmail.com>
2018-09-13 11:53:26 -0400

    Added log_in? and current_user functionality to be used in the layout view.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def logged_in?
+    return true if current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
