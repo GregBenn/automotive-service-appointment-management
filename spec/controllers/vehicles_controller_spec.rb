df208a3810a25733ef6a8fcd076de9c84127c9fd
Greg Benn <gbenn07@gmail.com>
2018-10-06 16:24:16 -0400

    Add controller action specs

--- spec/controllers/vehicles_controller_spec.rb
+++ spec/controllers/vehicles_controller_spec.rb
@@ -1,5 +1,11 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe VehiclesController, type: :controller do
-
-# end
+RSpec.describe VehiclesController, type: :controller do
+  describe "Vehicle Controller Actions" do
+    it "creates a new vehicle" do
+      create(:customer)
+      expect { create(:vehicle, customer_id: 1) }
+        .to change(Vehicle, :count).by(1)
+    end
+  end
+end
--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     name { "Bruce Wayne" }
     email { "ironmansucks@WayneCorp.com" }
     phone_number { "2156372212" }
+    id { 1 }
   end
 end
